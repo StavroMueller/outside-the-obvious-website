98cb613ede3f70d0d3a83755436efd9184edc247
Julien Clifford <jcliff90@gmail.com>
2023-01-02 16:35:02 -0600

    fix(utils): use env var

--- src/utilities/wotoFinder.js
+++ src/utilities/wotoFinder.js
@@ -2,7 +2,7 @@ const wotoFinder = (genre, numberOfPhotos) => {
     let photoUrls = []
     for (let i = 1; i <= numberOfPhotos; i++) {
         //Don't feel like writing a script to rename the files just yet, so lazy it is...
-        photoUrls.push(`%PUBLIC_URL%/gallery/${genre}/woto%20(${i}).jpg`);
+        photoUrls.push(`${process.env.PUBLIC_URL}/gallery/${genre}/woto%20(${i}).jpg`);
     }
 
     return photoUrls;
