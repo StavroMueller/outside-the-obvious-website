dbd9369c8af386b7ca73041cbbea9caddf8ae0ba
StavroMueller <jcliff90@gmail.com>
2023-01-29 11:53:34 -0600

    feat(src): add react-markdown, docs

---
+++ docs/feature_ideas.md
@@ -0,0 +1,5 @@
+# Feature Ideas
+
+## "Get this as a print"
+Each photo could have a link underneath that says "get this as a print", which then uses the photo url (`street/1.png`) as an ID to pass to a new page where they can quickly order a print in many sizes.
+
--- src/pages/reviews/RicohGRIII.js
+++ src/pages/reviews/RicohGRIII.js
@@ -5,6 +5,7 @@ const RicohGRIII = () => {
     return (
         <Container>
             <h1>Ricoh GR III</h1>
+            <p>The Ricoh GR is your every-present companion. With the ability to be slipped into a pocket, it is the perfect companion to any enthusiast camera; this small black brick simply always gets the job done.</p>
         </Container>
     )
 
