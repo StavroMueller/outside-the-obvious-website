c6fd75f03c36d00fa4539c3558be29740487ec46
Julien Clifford <jcliff90@gmail.com>
2025-06-17 15:47:09 -0500

    feat(home): improve homepage

--- src/pages/Root.js
+++ src/pages/Root.js
@@ -1,29 +1,33 @@
-import React from "react";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Typography from "@mui/material/Typography";
-import { SocialIcon } from "react-social-icons";
-import Gallery from "../components/Gallery";
-import wotoFinder from "../utilities/wotoFinder";
+import React from 'react';
+
+import {
+  Grid, 
+  Typography
+} from '@mui/material'
+import Gallery from '../components/Gallery';
+import wotoFinder from '../utilities/wotoFinder';
 
 const Root = () => {
-  const wotoUrls = [
-    ...wotoFinder("street", 3),
-    ...wotoFinder("fashion", 3),
-    ...wotoFinder("travel", 3),
-  ];
+  const wotoUrls = [...wotoFinder('street', 3), ...wotoFinder('fashion', 3), ...wotoFinder('travel', 3)];
   return (
     <>
-      <div>
-        <Container>
-          <Row></Row>
-          <Row></Row>
-          <Gallery imageUrls={wotoUrls} />
-        </Container>
-      </div>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography variant="h1">OUTSIDE THE OBVIOUS </Typography>
+      </Grid>
+    </Grid>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography variant="h3"> Photography with teeth. My photograpy whatever whatever whatever haha, this is just going to get out of hand, manFashion photography with teeth. I like it messy — the undone button, the smudged lipstick, the last drag before the spotlight hits. I shoot in the cracks between polish and instinct, where things still feel electric. Whether it’s backstage at fashion week or on the street in heels and sweat, I’m chasing energy, tension, and everything too good to plan.</Typography>
+      </Grid>
+    </Grid>
+    <Grid container>
+      <Grid item xs={12}>
+        <Typography variant="p"> <a href="mailto:contact@outsidetheobvious.com">contact@outsidetheobvious.com</a> <a href="https://www.instagram.com/outsidetheobvious">instagram</a></Typography>
+      </Grid>
+    </Grid>
     </>
-  );
-};
+  )
+}
 
-export default Root;
+export default Root
